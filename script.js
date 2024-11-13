0f71ec2350c08adf6617d46ef25084636c8d3e0c
Wesley Souza <wesleyondo@gmail.com>
2024-11-12 21:23:27 -0300

    app do aquario

---
+++ script.js
@@ -0,0 +1,26 @@
+/**
+ * Função para calcular o volume do aquário
+ * @author Wesley Souza
+ */
+function calcularVolume(event) {
+    event.preventDefault()
+
+    // Captura os valores de entrada
+    let comprimento = Number(document.getElementById('comprimento').value)
+    let largura = Number(document.getElementById('largura').value)
+    let altura = Number(document.getElementById('altura').value)
+
+    // Cálculo do volume
+    let volume = (comprimento * largura * altura) / 1000
+
+    // Exibição do resultado
+    document.getElementById('resultado').value = `${volume.toFixed(2)} litros`
+}
+
+// Função para limpar o campo de resultado ao resetar o formulário
+function limparResultado() {
+    document.getElementById('resultado').value = ''
+}
+
+// Adicionando evento para limpar o resultado ao resetar o formulário
+document.querySelector('form').addEventListener('reset', limparResultado);
